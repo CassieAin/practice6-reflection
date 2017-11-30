81367d83c9a8baadf080acb80dec51a6c1bb9a57
CassieAin <roksolana.d22@gmail.com>
2017-12-01 00:55:21 +0200

    MVC structure fix

--- src/task6_3/MainApplication.java
+++ src/task6_3/MainApplication.java
@@ -1,18 +1,12 @@
 package task6_3;
 
-import task6_3.controller.ClassManipulation;
-import task6_3.view.View;
+import task6_3.controller.MainController;
 
 import java.lang.reflect.*;
 
 public class MainApplication {
 
     public static void main(String [] args) throws ClassNotFoundException, NoSuchFieldException, NoSuchMethodException, InvocationTargetException, IllegalAccessException, InstantiationException {
-        View view = new View();
-        view.invokeToString();
-        view.invokeSetter();
-        view.invokeGetter();
-        view.invokeIncrement();
-        view.invokeDecrement();
+        MainController.runApplication();
     }
 }
--- src/task6_3/controller/MainController.java
+++ src/task6_3/controller/MainController.java
@@ -1,4 +1,15 @@
 package task6_3.controller;
 
+import task6_3.view.View;
+
 public class MainController {
+
+    public static void runApplication(){
+        View view = new View();
+        view.invokeToString();
+        view.invokeSetter();
+        view.invokeGetter();
+        view.invokeIncrement();
+        view.invokeDecrement();
+    }
 }
